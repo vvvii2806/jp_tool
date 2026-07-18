91de29c7f50fa9c37f7173e39964df8be573e132
Violeta <clemohermosilla@gmail.com>
2026-07-18 15:19:46 -0400

    main page structure progress

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from frontend.render_routes import render
 
-app = Flask(__name__, template_folder='frontend/templates')
+app = Flask(
+    __name__,
+    template_folder='frontend/templates',
+    static_folder='frontend/static'
+)
 
 render(app)
 
